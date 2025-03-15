3152f80eb7d0cfbd1353e45d62ecac6f1d02a555
Mayur Garg <mayur7garg@gmail.com>
2025-03-15 17:24:47 +0530

    Updated webpage

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Daily
+
+Something useful, something fun. Everyday.
+Visit [Daily - GitHub pages](https://mayur7garg.github.io/Daily/)
+
+> Developed by - Mayur Garg
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import json
 from datetime import date
 from pathlib import Path
-from random import choice
+from random import choice, randint
+
+def get_deck_date(d: date):
+    doy = d.timetuple().tm_yday - 1
+
+    if doy == 364:
+        deck_date = "Day of the Joker"
+    elif doy == 365:
+        deck_date = "Day of the Deck"
+    else:
+        planet = ["Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"][doy % 7]
+        suit = ["Spades", "Hearts", "Diamonds", "Clubs"][(doy // 7) % 4]
+        card = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"][(doy // 28) % 13]
+    
+        deck_date = f"{card} of {suit}, Day of {planet}"
+
+    return deck_date
 
 def get_word_meaning_of_the_day():
     with Path("data", "words.json").open(encoding = "utf-8") as f:
@@ -13,11 +29,15 @@ def get_word_meaning_of_the_day():
 with open("index.template.html", "r", encoding = "utf-8") as t:
     template = t.read()
 
+bg_color = f'style="background-color: rgb({randint(0, 127)}, {randint(0, 127)}, {randint(0, 127)});"'
 calendar_date = date.today().strftime("%A, %B %d, %Y")
+deck_date = get_deck_date(date.today())
 daily_word, daily_meaning = get_word_meaning_of_the_day()
 
 index_html = template.format(
+    bg_color = bg_color,
     calendar_date = calendar_date,
+    deck_date = deck_date,
     daily_word = daily_word,
     daily_meaning = daily_meaning
 )

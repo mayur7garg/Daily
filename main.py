14e508f7b8ef671f34df05dd562f391bb7120c5f
Mayur Garg <mayur7garg@gmail.com>
2025-03-23 16:15:29 +0530

    Added born today. Updated data.

--- main.py
+++ main.py
@@ -19,13 +19,25 @@ def get_deck_date(d: date):
 
     return deck_date
 
-def get_word_meaning_of_the_day():
+def get_word_meaning():
     with Path("data", "words.json").open(encoding = "utf-8") as f:
         words: dict = json.load(f)
 
     w = choice(list(words.keys()))
     return w, choice(words[w])
 
+def get_born_this_date(d: date):
+    date_key = d.strftime("%d-%m")
+
+    with Path("data", "dob.json").open(encoding = "utf-8") as f:
+        dob: dict = json.load(f)
+    
+    if date_key in dob:
+        name, year = choice(dob[date_key])
+        return f"{name} ({year})"
+
+    return "No record of someone born on this date"
+        
 def get_shlok():
     with Path("data", "Bhagwad_Gita.json").open(encoding = "utf-8") as f:
         shloks: dict = json.load(f)
@@ -37,9 +49,11 @@ with open("index.template.html", "r", encoding = "utf-8") as t:
     template = t.read()
 
 bg_color = f'style="background-color: rgb({randint(0, 150)}, {randint(0, 150)}, {randint(0, 150)});"'
-calendar_date = date.today().strftime("%A, %B %d, %Y")
-deck_date = get_deck_date(date.today())
-daily_word, daily_meaning = get_word_meaning_of_the_day()
+daily_date = date.today()
+calendar_date = daily_date.strftime("%A, %B %d, %Y")
+deck_date = get_deck_date(daily_date)
+daily_word, daily_meaning = get_word_meaning()
+born_this_date = get_born_this_date(daily_date)
 shlok = get_shlok()
 
 index_html = template.format(
@@ -48,6 +62,7 @@ index_html = template.format(
     deck_date = deck_date,
     daily_word = daily_word,
     daily_meaning = daily_meaning,
+    born_this_date = born_this_date,
     daily_shlok_chapter = shlok['Chapter'],
     daily_shlok_verse = shlok['Verse'],
     daily_shlok = shlok['Shloka'],
